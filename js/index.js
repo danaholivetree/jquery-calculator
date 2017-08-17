f8ba13731aa87c692d19f22c3233125b7475bfd7
danah olivetree <danah.olivetree@gmail.com>
2017-08-17 17:13:45 -0600

    fixed errors

--- js/index.js
+++ js/index.js
@@ -1,39 +1,41 @@
-$(function(){
+$(function() {
 
-var clear = 0
-// var txt = scr.html()
+  var clear = 0
+  // var txt = scr.html()
 
-$(".buttons").click(function(){
+  $("span").click(function() {
 
-  let scr = $('#screen')
-  let key= $( event.target).text()
-  if (clear === 1) {
-    scr.text('')
-    clear = 0
-  }
+    let scr = $('#screen')
+    let key = $(event.target).text()
+    if (clear === 1) {
+      scr.text('')
+      clear = 0
+    }
 
-  if (key==="C") {
-    scr.text('')
-  }
-  else if (key==="x"){
-    key = '*'
-    scr.text(scr.text()+(key))
-  }
-  else if (key==="÷") {
-    key = '/'
-    scr.text(scr.text()+(key))
-  }
-  else if (key === "\=") {
-    let res= eval(scr.text())
-    scr.text(res)
-    clear = 1
-  }
-  else {
-  scr.text(scr.text()+(key))
-  }
-})
+    if (key === "C") {
+      scr.text('')
+    } else if (key === "x") {
+      key = '*'
+      scr.text(scr.text() + (key))
+    } else if (key === "÷") {
+      key = '/'
+      scr.text(scr.text() + (key))
+    } else if (key === "\=") {
+      clear = 1
+      try {
+        eval(scr.text());
+      } catch (e) {
+        if (e instanceof SyntaxError) {
+          scr.text('ERROR')
+        } else {
+          throw (e);
+        }
 
-
-
-
+      }
+      let res = eval(scr.text())
+      scr.text(res)
+    } else {
+      scr.text(scr.text() + (key))
+    }
+  })
 })
